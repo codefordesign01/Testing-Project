788d3fe54d8b50cdfbbf595f8d2ff2cc68e0e0e4
Code For Design <93128026+codefordesign01@users.noreply.github.com>
2023-09-13 17:13:13 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace code_for_design.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
